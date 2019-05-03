f6b7003598f8ce47e0fad4f744aa2a7ba4a0a15f
Hayden Bradshaw <haydenbradshaw@Haydens-MacBook-Air.local>
2019-05-03 14:14:58 -0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+//array of objs containing pictures of fish with two correct answers attached
+// to do, find the proper img sizes, and how to attach
+// to do, exsplore attaching buttons to answers inside each obj
+//to do exsplore how to attach win condition to buttons for each button
+const questions = [{img1: 'answer1', 'answer2'},]
+const questions = {img2: 'answer1', 'answer2'},
+const questions = {img3: 'answer1', 'answer2'},
+const questions = {img4: 'answer1', 'answer2'},
+const questions = {img5: 'answer1', 'answer2'},
+const questions = {img6: 'answer1', 'answer2'},
+const questions = {img7: 'answer1', 'answer2'},
+const questions = {img8: 'answer1', 'answer2'},
+const questions = {img9: 'answer1', 'answer2'},
+const questions = {img10: 'answer1', 'answer2'}];
+
+//function feature to clear dom and there by start the next question
+function nextQuestion () {
+
+};
+
+//function to randomize object array on reload(clearing the dom)
+function clearDom () {
+
+};
+
+// event listners
+  document.querySelector('button').addEventListener('click', initialize);
+
+  //render? not sure where to start
+  
