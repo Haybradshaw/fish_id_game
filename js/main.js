e4705f70fa2c0a59e229003475b2af39d15895a2
Hayden Bradshaw <haydenbradshaw@Haydens-MacBook-Air.local>
2019-05-08 16:36:07 -0500

    new structure with SOME functonality

--- js/main.js
+++ js/main.js
@@ -1,105 +1,157 @@
 /*----- constants -----*/
-const redFish = {
-    '../picture/redfish.jpg',
-    option: [
-    {
-      isCorrect: false,
-      species: 'red snapper'
-    },
-    {
-      isCorrect: true,
-      species: 'red fish'
-    }
-  ]
-}
+const fishSpecies = [
+  { species: "redfish",
+    img: 'images/redfish.jpg',
+  }, 
+  { species: "ladyfish",
+    img: 'images/ladyfish.jpg',
+  }
+];
+
+//-----cache------
+const fish_img = document.getElementById('image');
+const next = document.getElementById('next');
+var answerOne = document.getElementById('answerOne')
+var answerTwo = document.getElementById('answerTwo')
 
 
-const ladyFish = {
-  src: '../picture/ladyfish.jpg',
-    option: [
-      {
-        isCorrect: true,
-        species: 'lady fish'
-      },
-      {
-        isCorrect: false,
-        species:'mullet'
-      }
-    ]
-}
+
+function handleClick() {
+    fish_img.src = '';
+    answerOne = '';
+    answerTwo = '';
+    render();
+};
+
+var x = 0;
+function nextFish(fishSpecies) {
+  fish_img.src = fishSpecies[x].img;
+  x += 1;
+};
+
+
+function render() {
+  nextFish(fishSpecies[x]);
+};
+
+next.addEventListener('click', handleClick);
+
+
+
+    
+
+// step 1
+// be able to print any of these on a page 
+// either create dynamically from js using createElement where these objects will render
+// or create skel div in html - access it - then innerHTML print the objects 
+
+// step 2 
+// connect button to get next fish 
+// create a test button that will allow you to nagivate through all your fishes 
+
+// step 3 
+// create your ansnwer buttons that have conditions that will meet your fish object
+// so if button1 === the species string 
+// on button2 (which is incorrect answer) - give a message saying try again or its wrong
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 /*----- app's state (variables) -----*/
-const questions = [redFish, ladyFish];
+// const FishArray = ['redFish', 'ladyFish'];
 
-let scoreKeeper = 0;
+// let scoreKeeper = 0;
 
-const questionCounter = [];
+// const questionCounter = [];
 
 
-/*----- cached element references -----*/
-document.getElementById('score').innerHTML = 'Score: ' + scoreKeeper;
+// /*----- cached element references -----*/
+// document.getElementById('score').innerHTML = 'Score: ' + scoreKeeper;
 
-//a new button will fade in that will allow the person to render the next question
+// //a new button will fade in that will allow the person to render the next question
 
-/*----- functions -----*/
+// /*----- functions -----*/
 
-function playQuestion() {
-  //showButtons();
-  const selectedQuestion = questions.pop();
-  questionCounter.push(selectedQuestion);
-  let imgElement = document.querySelector('.image')
-  imgElement.setAttribute('src', selectedQuestion.file)
-  document.getElementById('question-counter').innerHTML = 'Question ' + questionCounter.length + ' of 10';
+// function playQuestion() {
+//   //showButtons();
+//   const selectedQuestion = questions.pop();
+//   questionCounter.push(selectedQuestion);
   
-  renderButton('One', selectedQuestion.options[0].species)
-  renderButton('Two', selectedQuestion.options[1].species)
   
-}
+//   document.getElementById('question-counter').innerHTML = 'Question ' + questionCounter.length + ' of 10';
+  
+//   renderButton('One', selectedQuestion.options[0].species)
+//   renderButton('Two', selectedQuestion.options[1].species)
+  
+// }
 
-function renderButton(buttonNum, species) {
-  document.getElementById(`answer${buttonNum}`).value = 
- species;
-};  
+// function renderButton(buttonNum, species) {
+//   document.getElementById(`answer${buttonNum}`).value = 
+//  species;
+// };  
+// let fishtest = document.getElementsByClassName('.fishImg').src="../images/redfish.jpg";
+// // console.log(fishtest);
+// // playQuestion();
 
-//playQuestion();
+// //function hideButtons() {
+// //  document.getElementById('answerOne').style.visibility = 'hidden';
+// //  document.getElementById('answerTwo').style.visibility = 'hidden';
+// //}
 
-//function hideButtons() {
-//  document.getElementById('answerOne').style.visibility = 'hidden';
-//  document.getElementById('answerTwo').style.visibility = 'hidden';
-//}
+// //function showButtons() {
+// //  document.getElementById('answerOne').style.visibility = 'visible';
+// //  document.getElementById('answerTwo').style.visibility = 'visible';
+// //}
 
-//function showButtons() {
-//  document.getElementById('answerOne').style.visibility = 'visible';
-//  document.getElementById('answerTwo').style.visibility = 'visible';
-//}
+// /*----- event listeners -----*/
 
-/*----- event listeners -----*/
+// function feedBack() {
 
-function feedBack() {
-
-  document.getElementById('answerOne').addEventListener('click', function() {
-      hideButtons();
-      questionCounter[0].options[0].isCorrect === true  
-      ? (document.getElementById('embeddedMovie').innerHTML = questionCounter[0].correctVideoUrl,
-      document.getElementById('movieQuote').innerHTML = "CORRECT!")
-      : (document.getElementById('embeddedMovie').innerHTML = questionCounter[0].incorrectVideoUrl,
-      document.getElementById('movieQuote').innerHTML = "WRONG!");
-  });
-  document.getElementById('answerTwo').addEventListener('click', function() {
-      hideButtons();
-      questionCounter[0].options[1].isCorrect === true  
-      ? (document.getElementById('embeddedMovie').innerHTML = questionCounter[0].correctVideoUrl,
-      document.getElementById('movieQuote').innerHTML = "CORRECT!")
-      : (document.getElementById('embeddedMovie').innerHTML = questionCounter[0].incorrectVideoUrl,
-      document.getElementById('movieQuote').innerHTML = "WRONG!");
-  });
+//   document.getElementById('answerOne').addEventListener('click', function() {
+//       hideButtons();
+//       questionCounter[0].options[0].isCorrect === true  
+//       ? (document.getElementById('embeddedMovie').innerHTML = questionCounter[0].correctVideoUrl,
+//       document.getElementById('movieQuote').innerHTML = "CORRECT!")
+//       : (document.getElementById('embeddedMovie').innerHTML = questionCounter[0].incorrectVideoUrl,
+//       document.getElementById('movieQuote').innerHTML = "WRONG!");
+//   });
+//   document.getElementById('answerTwo').addEventListener('click', function() {
+//       hideButtons();
+//       questionCounter[0].options[1].isCorrect === true  
+//       ? (document.getElementById('embeddedMovie').innerHTML = questionCounter[0].correctVideoUrl,
+//       document.getElementById('movieQuote').innerHTML = "CORRECT!")
+//       : (document.getElementById('embeddedMovie').innerHTML = questionCounter[0].incorrectVideoUrl,
+//       document.getElementById('movieQuote').innerHTML = "WRONG!");
+//   });
   
 
-}
+// }
 
 
-function nextQuestion() {
+// function nextQuestion() {
 
-}
+// }
